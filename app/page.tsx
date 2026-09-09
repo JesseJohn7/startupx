410cbafcda17c330e0d6c364b5a471092d24349b
JesseJohn7 <Jessejohn260@gmail.com>
2026-09-09 22:28:00 +0100

    import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar"
 import Hero from "@/components/Hero"
+import Pricing from "@/components/Pricing"
 export default function Home() {
   return (
     <> 
