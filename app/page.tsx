567b5c103715f15f192986a0415a8638789ccbb7
JesseJohn7 <Jessejohn260@gmail.com>
2026-09-12 22:14:03 +0100

    testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,14 @@ import Applications from "@/components/Application"
 import Pricing from "@/components/Pricing"
 import Faqs from "@/components/Faqs"
 import Footer from "@/components/Footer";
-
+import Testimonials from "@/components/Testimonials";
 export default function Home() {
   return (
     <> 
     <Navbar />
     <Hero />
     <Applications />
+    <Testimonials/>
     <Pricing/>
     <Faqs/>
     <Footer/>
