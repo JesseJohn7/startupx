5553417d41e10f60f8bf85ca406ecc4fdfa4bed7
JesseJohn7 <Jessejohn260@gmail.com>
2026-09-12 18:43:58 +0100

    hero btns

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ export default function Hero() {
             <span>Read Success Stories</span>
             <svg
               width="20"
-              height="20"
+              height="30"
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +63,7 @@ export default function Hero() {
 
           <a
             href="#"
-            className="text-indigo-600 bg-indigo-100 px-5 py-2 rounded-full text-sm font-medium hover:bg-indigo-200 transition"
+            className="text-indigo-600 bg-indigo-100 px-8 py-4 rounded-full text-sm font-medium hover:bg-indigo-200 transition"
           >
             Get Started
           </a>
