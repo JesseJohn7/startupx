25ef65016d89846680c6ffdc6bb3bcad237aa72e
JesseJohn7 <Jessejohn260@gmail.com>
2026-09-12 19:19:19 +0100

    dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar"
 import Hero from "@/components/Hero"
-import Applications from "@/components/Applications"
+import Applications from "@/components/Application"
 import Pricing from "@/components/Pricing"
 export default function Home() {
   return (
